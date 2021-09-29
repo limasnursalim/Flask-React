c0e4263870edbe178a93750c9d78843b67793960
Limas Nursalim <limassnursalim@gmail.com>
2021-09-29 13:06:57 -0700

    my first REST API step 6

--- backend/sample_backend.py
+++ backend/sample_backend.py
@@ -55,12 +55,20 @@ def get_users():
     # return users
     if request.method == 'GET':
         search_username = request.args.get('name')
+        search_job = request.args.get('job')
         if search_username:
             subdict = {'users_list': []}
             for user in users['users_list']:
                 if user['name'] == search_username:
                     subdict['users_list'].append(user)
             return subdict
+        elif search_username and search_job:
+            subdict = {'users_list':[]}
+            for user in users['users_list']:
+                if user['name'] == search_username and user['job'] == search_job:
+                    subdict['users_list'].append(user)
+            return subdict
+
         return users
     elif request.method == 'POST':
         userToAdd = request.get_json()
@@ -70,12 +78,19 @@ def get_users():
         # 200 is the default code for a normal response
         return resp
 
-@app.route('/users/<id>')
+@app.route('/users/<id>', methods=['GET', 'DELETE'])
 def get_user(id):
-    if id:
-        for user in users['users_list']:
-            if user['id'] == id:
-                return user
-        return ({})
-    return users
+    if request.method == 'DELETE':
+        if id:
+            for user in users['users_list']:
+                if user['id'] == id:
+                    users['users_list'].remove(user)
+                    return users
+    if request.method == 'GET':
+        if id:
+            for user in users['users_list']:
+                if user['id'] == id:
+                    return user
+            return ({})
+        return users
 
